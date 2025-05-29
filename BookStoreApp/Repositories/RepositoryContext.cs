7cf9f487112c9911a181effd13d7e8ea4daeb553
Administrator <Administrator@DESKTOP-LCUT3UE>
2025-05-29 18:00:04 +0300

    Entity  model yapısı oluşturuldu, db context kaydı yapıldı.

---
+++ BookStoreApp/Repositories/RepositoryContext.cs
@@ -0,0 +1,10 @@
+﻿using BookStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
