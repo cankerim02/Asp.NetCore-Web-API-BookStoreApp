4a6cd34848c640c8aede5df6473c382b143197bb
cankerim02 <cankerim02@gmail.com>
2025-06-02 13:19:30 +0300

    refactoring

---
+++ BookStoreApp/Controllers/BooksController.cs
@@ -0,0 +1,13 @@
+﻿using BookStoreApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+    }
+}
