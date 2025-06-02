145afd46648cd8a75aa44566e1440f0d747268b0
Administrator <Administrator@DESKTOP-LCUT3UE>
2025-06-02 12:47:22 +0300

    Tip konfigrasyonu   veritabanında bu değişikliği migration da görebilmek için  model yapısı ezildi.

---
+++ BookStoreApp/Repositories/Config/BookConfig.cs
@@ -0,0 +1,18 @@
+﻿using BookStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStoreApp.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Karagöz ve Hacivat", Price = 75 },
+                new Book { Id = 2, Title = "Mesnevi", Price = 175 },
+                new Book { Id = 3, Title = "Devlet", Price = 375 }
+        );
+        }
+    }
+}
--- BookStoreApp/Repositories/RepositoryContext.cs
+++ BookStoreApp/Repositories/RepositoryContext.cs
@@ -1,10 +1,21 @@
 ﻿using BookStoreApp.Models;
+using BookStoreApp.Repositories.Config;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreApp.Repositories
 {
     public class RepositoryContext : DbContext
     {
+        public RepositoryContext(DbContextOptions options) : 
+            base(options)
+        {
+            
+        }
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
     }
 }
