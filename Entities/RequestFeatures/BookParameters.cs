8dd79053ff837ffeef783fa614e527efd9354ee8
cankerim02 <cankerim02@gmail.com>
2025-07-23 16:47:01 +0300

    BookParameter sınıfı  içerisine arama yapmak için searchTerm alanı eklendi.

--- Entities/RequestFeatures/BookParameters.cs
+++ Entities/RequestFeatures/BookParameters.cs
@@ -4,7 +4,9 @@
 	{
         public uint MinPrice { get; set; }
         public uint MaxPrice { get; set; } = 1000; 
-        public bool ValidPriceRange => MaxPrice > MinPrice; 
+        public bool ValidPriceRange => MaxPrice > MinPrice;
+
+        public String? SearchTerm { get; set; } // searching 
 
     }
 }
