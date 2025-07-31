217973bfe65cd370b48e7037b4744db63b1db60d
cankerim02 <cankerim02@gmail.com>
2025-07-31 11:25:41 +0300

    Sıralama özelliği için extension metot yazıldı.

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -1,7 +1,8 @@
 ﻿using Entities.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +25,49 @@ namespace Repositories.EFCore.Extensions
                 .Where(b => b.Title
                 .ToLower()
                 .Contains(searchTerm));
+        }
+
+        public static IQueryable<Book> Sort(this IQueryable<Book> books, string orderByQueryString)
+        {
+            if(string.IsNullOrWhiteSpace(orderByQueryString))
+                return books.OrderBy(b => b.Id);
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+
+
+            var propertyInfos = typeof(Book)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            
+            var orderQueryBuilder = new StringBuilder();
+
+
+            foreach (var param in orderParams)
+            {
+                if(string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(' ')[0];
+
+                var objectProperty = propertyInfos
+                    .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
+                    StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty is null)
+                    continue;
+
+                var direction = param.EndsWith("desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+
+                
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            if (orderQuery is null)
+                return books.OrderBy(b => b.Id);
+
+            return books.OrderBy(orderQuery);
         }
     }
 }
