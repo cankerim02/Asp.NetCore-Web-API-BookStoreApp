c91eb8667d5c1c80e4cbe02214442e34ca0d7b28
cankerim02 <cankerim02@gmail.com>
2025-10-07 10:29:39 +0300

    Controller tarafında GetAllBookAsync metoduna mediatype attribute uygulandı.

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -28,6 +28,7 @@ namespace Presentation.Controllers
         }
 
         [HttpGet]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
         public async Task<IActionResult> GetAllBooksAsync([FromQuery]BookParameters bookParameters)
         {
 
