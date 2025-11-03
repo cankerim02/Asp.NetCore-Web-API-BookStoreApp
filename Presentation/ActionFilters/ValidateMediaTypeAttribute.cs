0df8744b9e465b96cf6bad9733f8cf85ae25698c
cankerim02 <cankerim02@gmail.com>
2025-11-03 16:02:57 +0300

    mediatypeheadervalue kısmına ! operatör eklendi.

--- Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
+++ Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
@@ -28,7 +28,7 @@ namespace Presentation.ActionFilters
                 .Headers["Accept"]
                 .FirstOrDefault();
             
-            if(MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue? outMediaType))
+            if(!MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue? outMediaType))
             {
                 context.Result = new BadRequestObjectResult($"Media type not present. " + 
                     $"Please add Accept header with required media type.");
